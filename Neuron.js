949a4d845501c0522c380b322809327a2efc090b
archy_yu <archyyu321@gmail.com>
2024-12-08 18:19:30 +0000

    Create Neuron.js

---
+++ Neuron.js
@@ -0,0 +1,17 @@
+class Neuron {
+    constructor(nin) {
+        this.w = Array.from({ length: nin }, () => new Value(Math.random() * 2 - 1)); // Initialize weights
+        this.b = new Value(0); // Initialize bias
+    }
+
+    parameters() {
+        return [...this.w, this.b];
+    }
+
+    forward(x) {
+        // Compute the weighted sum plus bias
+        let act = this.w.reduce((acc, w, i) => acc.add(w.mul(x[i])), this.b);
+        // Apply the ReLU activation function
+        return act.relu();
+    }
+}
